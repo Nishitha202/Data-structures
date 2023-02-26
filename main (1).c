2a7e42c7435c8218a5b257e51543c91f9555bffd
Nishitha <121636893+Nishitha202@users.noreply.github.com>
2023-02-26 13:46:57 +0530

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,75 @@
+
+
+#include <stdio.h>
+#define MAXSIZE 5
+int stack[MAXSIZE];
+int top=-1;
+void push(void);
+void pop(void);
+void display(void);
+void main()
+{
+int choice;
+while(1)
+{
+    printf("Stack operation");
+    printf("1.push\n");
+    printf("2.pop\n");
+    printf("3.display\n");
+    printf("4.Exit\n");
+    printf("\nenter your choice\n");
+    scanf("%d",&choice);
+    switch(choice)
+    {
+        case 1:push();
+        break;
+        case 2:pop();
+        break;
+        case 3:display();
+        break;
+        case 4: return;
+        default:printf("enter valid choice");
+    }
+}
+}
+void push()
+{
+    int c;
+    if(top==(MAXSIZE-1)){
+        printf("overflow");
+        return;
+    }
+    else{
+        printf("enter element");
+        scanf("%d",&c);
+        stack[++top]=c;
+    
+    }
+    return;
+}
+void pop()
+{
+    if(top==-1){
+        printf("underflow");
+    return;
+    }
+    else{
+        printf("poped element is %d",stack[top--]);
+    }
+}
+void display()
+{
+    int i;
+if(top==-1){
+    printf("underflow");
+    return;
+}
+else{
+    printf("the elements of stack are");
+    for(i=top;i>=0;i--)
+        printf("%d\n",stack[i]);
+    
+}
+    }
+
+
